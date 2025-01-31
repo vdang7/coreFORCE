eb6147b4d3b0b5b8cedfac7ce7ce5848cd2a420e
a <vietadang1@gmail.com>
2025-01-30 22:28:06 -0600

    Use brand colors on UI components

--- resources/js/Components/NavLink.vue
+++ resources/js/Components/NavLink.vue
@@ -1,6 +1,6 @@
 <script setup>
-import { computed } from 'vue';
-import { Link } from '@inertiajs/vue3';
+import { computed } from "vue";
+import { Link } from "@inertiajs/vue3";
 
 const props = defineProps({
     href: {
@@ -14,13 +14,13 @@ const props = defineProps({
 
 const classes = computed(() =>
     props.active
-        ? 'inline-flex items-center px-1 pt-1 border-b-2 border-indigo-400 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-indigo-700 transition duration-150 ease-in-out'
-        : 'inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out',
+        ? "inline-flex items-center px-1 py-1 text-sm font-medium leading-5 text-gray-900 focus:outline-none focus:border-primary-700 transition duration-150 ease-in-out"
+        : "inline-flex items-center px-1 py-1 text-sm font-medium leading-5 text-gray-500 hover:text-primary-700 hover:bg-primary-100 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out"
 );
 </script>
 
 <template>
-    <Link :href="href" :class="classes">
+    <Link :href="href" :class="classes" class="rounded-md px-2">
         <slot />
     </Link>
 </template>
